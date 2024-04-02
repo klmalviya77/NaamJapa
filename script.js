1044ef4944c170b4dcdf5493b89d6f862ba45c49
klmalviya77 <klmalviya77@gmail.com>
2024-04-02 11:11:41 +0530

    update

--- script.js
+++ script.js
@@ -50,3 +50,13 @@ var currentDivIndex = 0;
     if (performance.navigation.type === 1) {
         window.location.href = "index.html";
     }
+    
+        /* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
